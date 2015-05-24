14c8b312853ebcfd1901229ef6c4ba2304c227ea
jgesser <jgesser@gmail.com>
2015-05-24 16:10:15 +0100

    assignment 2 done

--- assignments/assignment2/src/vandy/mooc/utils/RequestMessage.java
+++ assignments/assignment2/src/vandy/mooc/utils/RequestMessage.java
@@ -6,58 +6,58 @@ import android.os.Message;
 import android.os.Messenger;
 
 /**
- * A thin facade around an Android Message that defines the schema of
- * a request from the Activity to the Service.
+ * A thin facade around an Android Message that defines the schema of a request from the Activity to the Service.
  */
 public class RequestMessage extends RequestReplyMessageBase {
-    /**
-     * Constructor is private to ensure the makeRequestMessage()
-     * factory method is used.
-     */
-    private RequestMessage(Message message) {
-        super(message);
-    }
+	/**
+	 * Constructor is private to ensure the makeRequestMessage() factory method is used.
+	 */
+	private RequestMessage(Message message) {
+		super(message);
+	}
 
-    /**
-     * Convert a Message into a RequestMessage.
-     */
-    public static RequestMessage makeRequestMessage(Message message) {
-        // Make a copy of @a message since it may be recycled.
-        return new RequestMessage(Message.obtain(message));
-    }
+	/**
+	 * Convert a Message into a RequestMessage.
+	 */
+	public static RequestMessage makeRequestMessage(Message message) {
+		// Make a copy of @a message since it may be recycled.
+		return new RequestMessage(Message.obtain(message));
+	}
 
-    /**
-     * Factory method creates a RequestMessage to return to the
-     * Activity with information necessary to download an image.
-     */
-    public static RequestMessage makeRequestMessage(int requestCode, 
-                                                    Uri url,
-                                                    String directoryPathname,
-                                                    Messenger replyMessenger) {
-        // Create a RequestMessage that holds a reference to a Message
-        // created via the Message.obtain() factory method.
-        RequestMessage requestMessage =
-            new RequestMessage(Message.obtain());
+	/**
+	 * Factory method creates a RequestMessage to return to the Activity with information necessary to download an
+	 * image.
+	 */
+	public static RequestMessage makeRequestMessage(int requestCode, Uri url, String directoryPathname, Messenger replyMessenger) {
+		// Create a RequestMessage that holds a reference to a Message
+		// created via the Message.obtain() factory method.
+		RequestMessage requestMessage = new RequestMessage(Message.obtain());
 
-        // Store replyMessenger into the Message's replyTo field.
-        // TODO -- you fill in here.
+		// Store replyMessenger into the Message's replyTo field.
+		// TODOne -- you fill in here.
+		requestMessage.getMessage().replyTo = replyMessenger;
 
-        // Create a new Bundle to handle the result.
-        // TODO -- you fill in here.
+		// Create a new Bundle to handle the result.
+		// TODOne -- you fill in here.
+		Bundle bundle = new Bundle();
 
-        // Set the Bundle as the "data" for the Message.
-        // TODO -- you fill in here.
+		// Set the Bundle as the "data" for the Message.
+		// TODOne -- you fill in here.
+		requestMessage.setData(bundle);
 
-        // Put the URL to the image file into the Bundle
-        // TODO -- you fill in here.
+		// Put the URL to the image file into the Bundle
+		// TODOne -- you fill in here.
+		requestMessage.setImageURL(url);
 
-        // Put the pathname to the image file into the Bundle
-        // TODO -- you fill in here.
+		// Put the pathname to the image file into the Bundle
+		// TODOne -- you fill in here.
+		requestMessage.setDirectoryPathname(directoryPathname);
 
-        // Put the request code into the Bundle
-        // TODO -- you fill in here.
+		// Put the request code into the Bundle
+		// TODOne -- you fill in here.
+		requestMessage.setRequestCode(requestCode);
 
-        // Return the message to the caller.
-        return requestMessage;
-    }
+		// Return the message to the caller.
+		return requestMessage;
+	}
 }
